36a5cd2f7e32d7801c0236ec7122479bf8244da1
OlgaBeliaeva <olgabel505@web.de>
2024-10-03 15:10:19 +0200

    structure

---
+++ src/components/Article/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import styles from './styles.module.css';
+
+export default Article;
---
+++ src/components/Articles/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './styles.module.css';
+
+
+export default Articles;
